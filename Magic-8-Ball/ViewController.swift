ec29d68103a834bcfcf52af1d7a88e244cfddcb1
SudhakarH <sudhakarh1999@gmail.com>
2020-05-14 14:55:15 +0530

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by sudhakar H on 14/05/20.
+//  Copyright © 2020 sudhakar H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
